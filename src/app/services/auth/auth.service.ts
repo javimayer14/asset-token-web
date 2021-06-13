204cafe09c1049c156f3ec01586c9148da8658d9
fenevado <federico.nevado@gmail.com>
2021-06-13 00:51:02 -0300

    Corrección login

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -10,8 +10,8 @@ export class AuthService {
   constructor(private http: HttpClient, private router: Router, private serializer: UrlSerializer) { }
 
   public login(username: string, password: string) : Observable<any> {
-    const tree = this.router.createUrlTree(['api', 'oauth', 'token'])
-    const url = this.serializer.serialize(tree);
+    const tree = this.router.createUrlTree(['oauth','token'])
+    const url = 'http://localhost:8080'+this.serializer.serialize(tree);
     const headers = new HttpHeaders({
       'Authorization': 'Basic YW5ndWxhckFwcDoxMjM0NQ==',
       'Content-Type': 'application/x-www-form-urlencoded' }
